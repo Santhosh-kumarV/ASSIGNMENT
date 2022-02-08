3eebc0cecefe0e75bfeb05e7fecbe89f1a10c8bb
Santhosh-kumarV <99229396+Santhosh-kumarV@users.noreply.github.com>
2022-02-08 12:46:46 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,23 +4,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace starpattern
+namespace Factorial
 {
-    class Program
-    {
-        static void Main(string[] args)
+   
+        class Program
         {
-            for(int i=1;i<=10;i++)
+            static void Main(string[] args)
             {
-                for(int j=1;j<=i;j++)
-                {
-                    Console.Write("*");
-        
-                }
-                Console.WriteLine();
-            
+                Console.WriteLine("Enter number to find sin value:");
+                float num = Convert.ToSingle(Console.ReadLine());
+                SinOfNumber son = new SinOfNumber();
+                float sin = son.sin(num);
+                Console.WriteLine("Sin value of " + num + "is:" + sin);
+                Console.ReadLine();
             }
-            Console.ReadLine();
+        }
+        class SinOfNumber
+        {
+            int factorial(int fact)
+            {
+                int i;
+                int k = 1;
+                for (i = 1; i <= fact; i++)
+                {
+                    k = k * i;
+                }
+                return k;
+            }
+            float Pow(float num, int pow)
+            {
+                float result = 1;
+
+                if (pow > 0)
+                {
+                    for (int i = 1; i <= pow; ++i)
+                    {
+                        result *= num;
+                    }
+                }
+                else if (pow < 0)
+                {
+                    for (int i = -1; i >= pow; --i)
+                    {
+                        result /= num;
+                    }
+                }
+
+                return result;
+            }
+            public float sin(float num)
+            {
+                float sum = 0;
+                int i, j = 0;
+                for (i = 0; i <= num + 10; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        j++;
+                        float numaretor = Pow(num, i);
+                        float denominator = factorial(i);
+                        float term = numaretor / denominator;
+                        if (j % 2 != 0)
+                        {
+                            sum = sum + term;
+                        }
+                        else
+                            sum = sum - term;
+                    }
+                }
+                return sum;
+            }
         }
     }
-}
+
