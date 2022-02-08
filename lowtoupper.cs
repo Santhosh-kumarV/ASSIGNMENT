54f6899b4cd0ca983becf2bf6f25a744a59ad23b
Santhosh-kumarV <99229396+Santhosh-kumarV@users.noreply.github.com>
2022-02-08 14:56:14 +0530

    Add files via upload

---
+++ lowtoupper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lowtoupper
+{
+    using System;
+    using System.IO;
+    namespace FileLowerCaseToUpperCase
+    {
+        class lowtoupper
+        {
+            static void Main()
+            {
+                Console.Write("Enter name file: ");
+                string fileName = Console.ReadLine();
+
+                if (File.Exists(fileName))
+                {
+                    StreamReader fileRw = File.OpenText(fileName);
+                    StreamWriter fileWr = File.CreateText(fileName + ".dat");
+                    string line;
+                    do
+                    {
+                        line = fileRw.ReadLine();
+                        if (line != null)
+                            fileWr.WriteLine(line.ToUpper());
+                    }
+                    while (line != null);
+                    fileRw.Close();
+                    fileWr.Close();
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
