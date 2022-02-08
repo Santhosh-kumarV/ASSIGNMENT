9d72e9c6c04d992f6e51d6d0662449374f6b100d
Santhosh-kumarV <99229396+Santhosh-kumarV@users.noreply.github.com>
2022-02-08 12:45:20 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,62 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace currentdate
+namespace starpattern
 {
-    
-        class CalChecking
+    class Program
+    {
+        static void Main(string[] args)
         {
-            static bool isLeap(int year)
+            for(int i=1;i<=10;i++)
             {
-                return (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0));
-            }
-
-            static bool isdat()
-            {
-                Console.WriteLine(" DAY:");
-                int Day = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("MONTH:");
-                int Month = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("YEAR:");
-                int Year = Convert.ToInt32(Console.ReadLine());
-
-                if (Year > 9999 || Year < 1500)
-                    return false;
-
-                if (Month < 1 || Month > 12)
-                    return false;
-
-
-                if (Day < 1 || Day > 31)
-                    return false;
-
-                if (Month == 2)
+                for(int j=1;j<=i;j++)
                 {
-                    if (isLeap(Year))
-                        return (Day <= 29);
-                    else
-                        return (Day <= 28);
+                    Console.Write("*");
+        
                 }
-
-                if (Month == 4 || Month == 6 || Month == 9 || Month == 11)
-                    return (Day <= 30);
-
-                return true;
+                Console.WriteLine();
+            
             }
-
-            public static void Main(string[] args)
-            {
-                if (isdat())
-                {
-                    Console.WriteLine("The date is ValidDate");
-                }
-                else
-
-                    Console.WriteLine("The date is invalid date");
             Console.ReadLine();
-            }
         }
-
     }
-
-
+}
