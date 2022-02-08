fa961e59bbccff4b52e18145fe5b442efe54e7ff
Santhosh-kumarV <99229396+Santhosh-kumarV@users.noreply.github.com>
2022-02-08 14:57:40 +0530

    Add files via upload

---
+++ multi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("enter the number:");
+          
+           int n = Convert.ToInt32(Console.ReadLine());
+            int i = 1;
+            while (i <= 10)
+            {
+                int x = n * i;
+
+
+                Console.WriteLine(n + "*" + i + "=" + x);
+                i++;
+
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
