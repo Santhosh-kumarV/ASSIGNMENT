5b8e73809497d049fad661f648dfcbb203b434f9
Santhosh-kumarV <99229396+Santhosh-kumarV@users.noreply.github.com>
2022-02-08 12:45:47 +0530

    Add files via upload

---
+++ leafyear.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace starpattern
+{
+    class leafyear
+    {
+        static void main(string[]args)
+        {
+            bool leaforNot = false;
+            Console.WriteLine("enter a year");
+            int y = Convert.ToInt32(Console.ReadLine());
+            if(y%4==0&&y%100!=0)
+            {
+                Console.WriteLine("leaf year");
+            }
+            else if(y%100==0&&y%400==0)
+            {
+                Console.WriteLine("leaf year");
+            }
+            else
+            {
+                Console.WriteLine("not a leaf year");
+            }
+            Console.ReadLine();
+        }
+    }
+}
