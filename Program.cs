012482d469e17e889ad984ec56199ed121ad2091
Santhosh-kumarV <99229396+Santhosh-kumarV@users.noreply.github.com>
2022-02-08 12:48:17 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,18 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace House
+namespace kelvintofarenhit
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            
-                SmallApartment apartment = new SmallApartment();
-                Person person = new Person("vijay");
-                apartment.door = new House.Door("black");
-                person.house = apartment;
-                person.ShowData();
-                Console.ReadLine();
-            }        public class House        {            public int Area { get; set; }            public Door door;            public House(int area = 200)            {                Area = area;            }            public void ShowData()            {                Console.WriteLine($"I am a house, my area is {Area} m2");            }            public Door GetDoor()            {                return door;            }            public class Door            {                public string Color { get; set; }                public Door(string color = "white")                {                    Color = color;                }                public void ShowData()                {                    Console.WriteLine($"I am a door, my color is {Color}");                }            }        }        public class SmallApartment : House        {            public SmallApartment(int area = 50) : base(area)            {            }        }        public class Person        {            private string name;            public House house { get; set; }            public Person(string name)            {                this.name = name;            }            public void ShowData()            {                Console.WriteLine($"A person whose name is {name}");                Console.WriteLine("Data of house:");                house.ShowData();                Console.WriteLine("Data of door:");                house.GetDoor().ShowData();            }        }    }}
         
+        
+            static void Main(string[] args)
+            {
+                Console.WriteLine("enter the amount of celsius");
+                int celsius = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("kelvin={0}", celsius + 273);
+                Console.WriteLine("fahrenheit={0}", celsius * 18 / 10 + 32);
+                Console.ReadLine();
+            }
+        }
+    }
+
+
