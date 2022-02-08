a2b62d6d6ea68eac20d496543c13d059399eb31b
Santhosh-kumarV <99229396+Santhosh-kumarV@users.noreply.github.com>
2022-02-08 10:58:23 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csvfile
+{
+
+    class csvfile
+    {
+        static void Main(string[] args)
+        {
+            var reader = new StreamReader(File.OpenRead(@"D:\New folder\Data.csv"));
+            List<string> listA = new List<string>();
+            List<string> listB = new List<string>();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(';');
+
+                listA.Add(values[0]);
+                listB.Add(values[1]);
+                foreach (var coloumn1 in listA)
+                {
+                    Console.WriteLine(coloumn1);
+                }
+                foreach (var coloumn2 in listA)
+                {
+                    Console.WriteLine(coloumn2);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
+        
+
