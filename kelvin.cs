700b15b3a42576a270100e9b45aa16c56ad70cf4
Santhosh-kumarV <99229396+Santhosh-kumarV@users.noreply.github.com>
2022-02-08 14:12:34 +0530

    Add files via upload

---
+++ kelvin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kelvintofarenhit
+{
+    class Program
+    {
+        
+        
+            static void Main(string[] args)
+            {
+                Console.WriteLine("enter the amount of celsius");
+                int celsius = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("kelvin={0}", celsius + 273);
+                Console.WriteLine("fahrenheit={0}", celsius * 18 / 10 + 32);
+                Console.ReadLine();
+            }
+        }
+    }
+
+
